4a644bab04afe6843c4eebbe30079783a1e9d11c
marisatrtm <marisatriutami@outlook.com>
2018-12-09 16:56:04 +0700

    favorites, unfavorites, tapi belum bisa add restaurant to favoritePage

--- src/pages/discover/discover.ts
+++ src/pages/discover/discover.ts
@@ -1,15 +1,25 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from "angularfire2/database";
 
 @Component({
   selector: "page-discover",
   templateUrl: "discover.html"
 })
 export class DiscoverPage {
-  constructor(public navCtrl: NavController) {}
+  restaurantList: AngularFireObject<any>
+  restaurant:any
+  constructor(public navCtrl: NavController, public db: AngularFireDatabase) {
+    this.restaurantList = db.object('/restaurant/rest_1');
+    this.restaurant = this.restaurantList.valueChanges();
+  }
 
   // Display selected restaurant
-  openResto() {
-    this.navCtrl.push("RestoDetailsPage");
+  openResto(id) {
+    id='rest_1';
+    console.log(id);
+    this.navCtrl.push("RestoDetailsPage",{
+      data:id
+    });
   }
 }
--- src/pages/resto-details/resto-details.ts
+++ src/pages/resto-details/resto-details.ts
@@ -7,16 +7,70 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'resto-details.html',
 })
 export class RestoDetailsPage {
+<<<<<<< HEAD
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
+=======
+  favorites:AngularFireList<any>;
+  
+  restaurant:AngularFireObject<any>;
+  restaurantDetail:any;
+  
+  currentUser:any;
+  
+  isFavorite:boolean = false;
+  selectedRestaurantId:any; // current Restaurant Id. diambil dari page yang klik resto detail (random, discover, favorites)
+
+  newFavorite:any;
+  constructor(public navCtrl: NavController, private restoService: RestoService, public navParams: NavParams, public db: AngularFireDatabase, public authService:AuthService) {
+    this.favorites = db.list('/favorites');
+    
+    this.selectedRestaurantId = navParams.get('data'); //Terima restaurant ID dari discover/favorite
+    
+    this.restaurant = db.object('/restaurant/rest_1'); // ganti jadi parameter this.selectedRestaurantId
+    this.restaurantDetail = this.restaurant.valueChanges();
+  }
+
+  ionViewWillEnter() {
+    this.currentUser = this.authService.getActiveUser().uid;
+>>>>>>> favorites, unfavorites, tapi belum bisa add restaurant to favoritePage
     console.log('ionViewDidLoad RestoDetailsPage');
+  }
+
+  ionViewDidLoad(){
+    console.log(this.selectedRestaurantId);
   }
 
   // Display selected account
   openAccount() {
     this.navCtrl.push("AccountDetailsPage");
   }
+<<<<<<< HEAD
+=======
+
+  checkFavorite(){
+  }
+
+  favorite(){
+    return this.restoService.isFavorite2();
+  }
+
+  change(){
+    if( this.restoService.isFavorite2() == false) { // Favorite
+      
+      this.favorites.update(this.currentUser + "/restaurants/" + 'rest_2', // param terakhir ganti jadi variable yang this.selectedRestaurantId
+      { 
+        id: 'rest_2' // ganti jadi this.selectedRestaurantId
+      })
+    }
+    else { // Unfavorite
+      const currentFavorite = this.db.list('/favorites/' + this.currentUser + '/restaurants/' + 'rest_2'); // rest_2 ganti jadi this.selectedRestaurantId
+      currentFavorite.remove();
+    }
+    return this.restoService.change();
+  }
+>>>>>>> favorites, unfavorites, tapi belum bisa add restaurant to favoritePage
 }
