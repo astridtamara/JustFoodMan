e7ff032cba6d91eff123fe1da72451fcb04d839b
Astrid Tamara <astrid.tamara@hotmail.com>
2018-12-10 02:58:50 +0700

    Modify Favorites Page

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,49 +1,42 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { RestoService } from '../../service/RestoService';
-import { Restaurant } from '../../data/restaurant.interface';
-import { Http } from '@angular/http';
-import { AuthService } from '../../service/AuthService';
- 
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+import { AuthService } from "../../service/AuthService";
+
 @Component({
-  selector: 'page-favorites',
-  templateUrl: 'favorites.html'
+  selector: "page-favorites",
+  templateUrl: "favorites.html"
 })
 export class FavoritesPage {
-  restoCollection:Restaurant[];
-  favoriteList:AngularFireList<any>;
-  favorite: any;
-  currentUser:any;
+  favoriteList: AngularFireList<any>;
+  favorites: any;
+  currentUser: any;
 
-  constructor(public http: Http, private restoService: RestoService, public db: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams, public authService:AuthService) {
+  constructor(
+    public afDatabase: AngularFireDatabase,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public authService: AuthService
+  ) {
     this.currentUser = authService.getActiveUser().uid;
-    this.favoriteList = this.db.list('/favorites/'+this.currentUser+'/restaurants');
-    this.favorite = this.favoriteList.valueChanges();
+    this.favoriteList = this.afDatabase.list(
+      "/favorites/" + this.currentUser + "/restaurants"
+    );
+    this.favorites = this.favoriteList.snapshotChanges().map(snapshots =>
+      snapshots.map(data => ({
+        data: this.afDatabase
+          .object("restaurant/" + data.payload.val().id)
+          .valueChanges()
+      }))
+    );
   }
- 
+
   // Display selected restaurant
-  openResto() {
-    this.navCtrl.push("RestoDetailsPage");
-  } 
-
-  ionViewWillEnter(){
-    this.restoCollection = this.restoService.getFavoriteResto();
+  openResto(restoID: string) {
+    this.navCtrl.push("RestoDetailsPage", { data: restoID });
   }
 
-  onAddResto(restaurant:Restaurant){
-    this.restoService.addRestoToFavorite(restaurant);
-  }
-
-  showResto(restaurant:Restaurant) {
-    console.log(restaurant);
-  }
-
-  isFavorite(){
-    return this.restoService.isFavorite2();
-  }
-
-  change(){
-   return this.restoService.change();
+  removeFavorite(restoID: string) {
+    this.favoriteList.remove(restoID);
   }
 }
--- src/pages/resto-details/resto-details.ts
+++ src/pages/resto-details/resto-details.ts
@@ -1,38 +1,53 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject
+} from "angularfire2/database";
+
+import { AuthService } from "../../service/AuthService";
+import { RestoService } from "../../service/RestoService";
 
 @IonicPage()
 @Component({
-  selector: 'page-resto-details',
-  templateUrl: 'resto-details.html',
+  selector: "page-resto-details",
+  templateUrl: "resto-details.html"
 })
 export class RestoDetailsPage {
-  favorites:AngularFireList<any>;
-  
-  restaurant:AngularFireObject<any>;
-  restaurantDetail:any;
-  
-  currentUser:any;
-  
-  isFavorite:boolean = false;
-  selectedRestaurantId:any; // current Restaurant Id. diambil dari page yang klik resto detail (random, discover, favorites)
+  favorites: AngularFireList<any>;
 
-  newFavorite:any;
-  constructor(public navCtrl: NavController, private restoService: RestoService, public navParams: NavParams, public db: AngularFireDatabase, public authService:AuthService) {
-    this.favorites = db.list('/favorites');
-    
-    this.selectedRestaurantId = navParams.get('data'); //Terima restaurant ID dari discover/favorite
-    
-    this.restaurant = db.object('/restaurant/rest_1'); // ganti jadi parameter this.selectedRestaurantId
+  restaurant: AngularFireObject<any>;
+  restaurantDetail: any;
+
+  currentUser: any;
+
+  isFavorite: boolean = false;
+  selectedRestaurantId: any; // current Restaurant Id. diambil dari page yang klik resto detail (random, discover, favorites)
+
+  newFavorite: any;
+  
+  constructor(
+    public navCtrl: NavController,
+    private restoService: RestoService,
+    public navParams: NavParams,
+    public db: AngularFireDatabase,
+    public authService: AuthService
+  ) {
+    this.favorites = db.list("/favorites");
+
+    this.selectedRestaurantId = navParams.get("data"); //Terima restaurant ID dari discover/favorite
+
+    this.restaurant = db.object("/restaurant/rest_1"); // ganti jadi parameter this.selectedRestaurantId
     this.restaurantDetail = this.restaurant.valueChanges();
   }
 
   ionViewWillEnter() {
     this.currentUser = this.authService.getActiveUser().uid;
-    console.log('ionViewDidLoad RestoDetailsPage');
+    console.log("ionViewDidLoad RestoDetailsPage");
   }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     console.log(this.selectedRestaurantId);
   }
 
@@ -41,23 +56,27 @@ export class RestoDetailsPage {
     this.navCtrl.push("AccountDetailsPage");
   }
 
-  checkFavorite(){
-  }
+  checkFavorite() {}
 
-  favorite(){
+  favorite() {
     return this.restoService.isFavorite2();
   }
 
-  change(){
-    if( this.restoService.isFavorite2() == false) { // Favorite
-      
-      this.favorites.update(this.currentUser + "/restaurants/" + 'rest_2', // param terakhir ganti jadi variable yang this.selectedRestaurantId
-      { 
-        id: 'rest_2' // ganti jadi this.selectedRestaurantId
-      })
-    }
-    else { // Unfavorite
-      const currentFavorite = this.db.list('/favorites/' + this.currentUser + '/restaurants/' + 'rest_2'); // rest_2 ganti jadi this.selectedRestaurantId
+  change() {
+    if (this.restoService.isFavorite2() == false) {
+      // Favorite
+
+      this.favorites.update(
+        this.currentUser + "/restaurants/" + "rest_2", // param terakhir ganti jadi variable yang this.selectedRestaurantId
+        {
+          id: "rest_2" // ganti jadi this.selectedRestaurantId
+        }
+      );
+    } else {
+      // Unfavorite
+      const currentFavorite = this.db.list(
+        "/favorites/" + this.currentUser + "/restaurants/" + "rest_2"
+      ); // rest_2 ganti jadi this.selectedRestaurantId
       currentFavorite.remove();
     }
     return this.restoService.change();
